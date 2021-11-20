c354f76d073fe9b5b1dfd141e8495d3ac932273a
Alan Lengkoan <alanlengkoan15@gmail.com>
2021-11-20 10:20:13 +0800

    update portfolio

--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-// PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../../sw.js')
-      .then((reg) => {
-        // console.log('Service worker registered.', reg);
-      });
-  });
-}
